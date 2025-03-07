08c5909779f1791d19a1a27923fb6ae6e9af65f9
grayfoxninguno <grayfoxninguno@gmail.com>
2025-03-07 23:22:45 +0100

    contacto empezado

--- eventos.js
+++ eventos.js
@@ -16,4 +16,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     actualizarPadding();
     window.addEventListener("resize", actualizarPadding);
+
+    document.getElementById("emailForm").addEventListener("submit", function(event) {
+        event.preventDefault(); 
+    
+        let email = document.getElementById("email").value;
+        let message = document.getElementById("message").value;
+        let recipient = "destinatario@example.com"; // correo de destino
+    
+        let mailtoLink = `mailto:${recipient}?subject=Mensaje de ${email}&body=${encodeURIComponent(message)}`;
+        
+        window.location.href = mailtoLink;
+    });
+    
 });
