b9bf904c0cf991ade2410382c6d021c03eea175b
grayfoxninguno <grayfoxninguno@gmail.com>
2025-03-11 13:16:57 +0100

    footer medio hecho

--- eventos.js
+++ eventos.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     const header = document.getElementById("header");
-    const relativeContainer = document.querySelector(".relative-container");
-
     window.addEventListener("scroll", function () {
         if (window.scrollY > 50) {
             header.classList.add("shrink");
