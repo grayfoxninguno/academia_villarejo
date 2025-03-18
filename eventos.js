9abc5ef39463ec317333d8bac49ac2ac8b6d3494
grayfoxninguno <grayfoxninguno@gmail.com>
2025-03-18 15:19:39 +0100

    Arrreglado el footer, hecho sobre nosotros , mejorado el contacto, cosas generales retocadas.

--- eventos.js
+++ eventos.js
@@ -1,30 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const header = document.getElementById("header");
-    window.addEventListener("scroll", function () {
-        if (window.scrollY > 50) {
-            header.classList.add("shrink");
-        } else {
-            header.classList.remove("shrink");
-        }
-    });
-    
-    function actualizarPadding() {
-        document.documentElement.style.setProperty('--header-height', header.offsetHeight + 'px');
+  const header = document.getElementById("header");
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 50) {
+      header.classList.add("shrink");
+    } else {
+      header.classList.remove("shrink");
     }
+  });
 
-    actualizarPadding();
-    window.addEventListener("resize", actualizarPadding);
+  function actualizarPadding() {
+    document.documentElement.style.setProperty(
+      "--header-height",
+      header.offsetHeight + "px"
+    );
+  }
 
-    document.getElementById("emailForm").addEventListener("submit", function(event) {
-        event.preventDefault(); 
-    
-        let email = document.getElementById("email").value;
-        let message = document.getElementById("message").value;
-        let recipient = "destinatario@example.com"; // correo de destino
-    
-        let mailtoLink = `mailto:${recipient}?subject=Mensaje de ${email}&body=${encodeURIComponent(message)}`;
-        
-        window.location.href = mailtoLink;
+  actualizarPadding();
+  window.addEventListener("resize", actualizarPadding);
+
+  document
+    .getElementById("emailForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      let email = document.getElementById("email").value;
+      let message = document.getElementById("message").value;
+      let recipient = "destinatario@example.com"; // correo de destino
+
+      let mailtoLink = `mailto:${recipient}?subject=Mensaje de ${email}&body=${encodeURIComponent(
+        message
+      )}`;
+
+      window.location.href = mailtoLink;
     });
-    
 });
