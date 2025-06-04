96f31fe401649f49e32e68f7af95fdbbdc77c49d
Gray <grayfoxninguno@gmail.com>
2025-06-05 01:47:55 +0200

    Convertido academia_villarejo en parte del proyecto principal

--- academia_villarejo/eventos.js
+++ academia_villarejo/eventos.js
@@ -34,3 +34,43 @@ document.addEventListener("DOMContentLoaded", function () {
       window.location.href = mailtoLink;
     });
 });
+
+/* Menu desplegable */
+const menu = document.querySelector('.nav-links');
+const toggleBtn = document.querySelector(".menu-toggle");
+
+// Mostrar menu
+toggleBtn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
+
+document.addEventListener("click", (e) => {
+  // Si el clic fue fuera del menú y del botón
+  if (!menu.contains(e.target) && !toggleBtn.contains(e.target)) {
+    menu.classList.remove("show");
+  }
+});
+
+
+/*Carousel*/
+const carrusel = document.getElementById("carouselImages");
+const total = carrusel.children.length;
+let indice = 0;
+
+function moverCarrusel(direccion) {
+  indice = (indice + direccion + total) % total;
+  actualizarCarrusel();
+}
+
+function actualizarCarrusel() {
+  carrusel.style.transform = `translateX(-${indice * 100}%)`;
+}
+
+function abrirModal(src) {
+  document.getElementById("modalImg").src = src;
+  document.getElementById("modal").style.display = "flex";
+}
+
+function cerrarModal() {
+  document.getElementById("modal").style.display = "none";
+}
